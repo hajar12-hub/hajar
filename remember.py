b0522ed177ca47cf949be970cca848eeb82eccad
hajar12-hub <azaouhajar@gmail.com>
2024-12-03 23:26:32 +0100

    Add files via upload

---
+++ remember.py
@@ -0,0 +1,10 @@
+print("les nombres premier sont :")
+for i in range (2,20):
+    prime = True
+    for j in range (2,20):
+       if i%j==0:
+          prime =False
+          break;
+    if :
+       print(i)
+
